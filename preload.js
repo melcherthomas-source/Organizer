39b6d1aac1225ffaa680f8f70d26cd2e59a71a0e
melcherthomas-source <melcherthomas@icloud.com>
2026-09-10 16:31:46 +0200

    Fix Outlook drag-and-drop file path for Electron 38

--- preload.js
+++ preload.js
@@ -1,6 +1,7 @@
-const {contextBridge,ipcRenderer}=require('electron');
+const {contextBridge,ipcRenderer,webUtils}=require('electron');
 contextBridge.exposeInMainWorld('desktopAPI',{
  inspectMailFile:p=>ipcRenderer.invoke('inspect-mail-file',p),
  saveDroppedFile:p=>ipcRenderer.invoke('save-dropped-file',p),
- openAttachment:p=>ipcRenderer.invoke('open-attachment',p)
+ openAttachment:p=>ipcRenderer.invoke('open-attachment',p),
+ getPathForFile:file=>webUtils.getPathForFile(file)
 });
